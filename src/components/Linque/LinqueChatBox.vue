9845c24071cb54fb8b5c598df5f48aa9b62add85
hellfreezes <hellfreezes@yandex.ru>
2021-04-05 06:53:30 +0700

    message

--- src/components/Linque/LinqueChatBox.vue
+++ src/components/Linque/LinqueChatBox.vue
@@ -1,19 +1,39 @@
 <template>
   <div class="content">
-    ChatBox
+    <LinqueMessage
+      v-for="(message, index) in messages"
+      :key="index"
+      :name="message.name"
+      :owner="false"
+      :text="message.text"
+    />
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
+import LinqueMessage from './LinqueMessage';
+
 export default defineComponent({
-name: "LinqueChatBox"
+  name: 'LinqueChatBox',
+  components: {
+    LinqueMessage,
+  },
+  data: () => ({
+    messages: [
+      {
+        name: 'Me',
+        owner: true,
+        text: 'This is a message',
+      },
+    ],
+  }),
 });
 </script>
 
 <style scoped>
-  .content {
-    padding: 5px;
-  }
+.content {
+  padding: 5px;
+}
 </style>
